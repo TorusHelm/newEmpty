68f81ad86f5885d16abca2b9e4a9962733835db8
TorusHelm <TorusHelm@list.ru>
2019-02-22 19:22:40 +0500

    remote

--- _dev/js/modules/showPass.js
+++
@@ -1,8 +0,0 @@
-export function showPass(){
-  let link = $('.js-view-pass');
-  link.on('click', function(){
-    let inp = $(this).siblings();
-    let inpType = inp.prop('type');
-    (inpType === 'password') ? inp.prop('type', 'text') : inp.prop('type', 'password');
-  })
-}
